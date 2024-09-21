49fae696120240e42b7bbd9c1669bc46902b5be4
trojvn <mrx.trojan@gmail.com>
2024-09-21 15:03:57 +0300

    with playwright

--- envs.py
+++ envs.py
@@ -2,7 +2,5 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
-DB_NAME = os.getenv("DB_NAME", "")
-DB_USER = os.getenv("DB_USER", "")
-DB_PSWD = os.getenv("DB_PSWD", "")
+load_dotenv("env.txt")
+URL = os.getenv("URL", "")
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from playwright.sync_api import sync_playwright
+
+from envs import URL
+
+
+def _main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(URL)
+        browser.close()
+
+
 def main():
     pass
 
