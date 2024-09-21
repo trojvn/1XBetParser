5a09276915d1e2349c4990953a62816fd20068c2
trojvn <mrx.trojan@gmail.com>
2024-09-21 15:19:55 +0300

    refactor

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from envs import URL
 
 def _main():
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(URL)
+        page.wait_for_timeout(3000)
+        _xpath = "xpath=//li[contains(@class,'dashboard-game')]"
+        elements = page.query_selector_all(_xpath)
+        print(elements)
         browser.close()
 
 
