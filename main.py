47ac40458e9ef0402dd9697651f8f5040a7263a3
trojvn <mrx.trojan@gmail.com>
2024-09-21 16:00:32 +0300

    refactor

--- main.py
+++ main.py
@@ -3,22 +3,31 @@ from playwright.sync_api import sync_playwright
 from envs import URL
 
 
-def _main():
+def __main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto(URL)
-        page.wait_for_timeout(3000)
-        _xpath = "//li[contains(@class,'dashboard-game')]/*[contains(@class,'dashboard-markets')]//button/span[contains(@text,'')]"
-        elements = page.query_selector_all(_xpath)
-        for element in elements:
-            text = element.inner_text()
-            print(text)
-        browser.close()
+        _main(p)
+
+
+def _main(p):
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto(URL)
+    page.wait_for_timeout(3000)
+    _xpath = "//li[contains(@class,'dashboard-game')]/*[contains(@class,'dashboard-markets')]//button/span[contains(@text,'')]"
+    elements_cf = page.query_selector_all(_xpath)
+    _xpath = "//li[contains(@class,'dashboard-game')]/*[contains(@class,'dashboard-game-block')]//a"
+    elements_url = page.query_selector_all(_xpath)
+    for element in elements_url:
+        href = element.get_attribute("href")
+        cf1 = elements_cf.pop().inner_text()
+        _ = elements_cf.pop()
+        cf2 = elements_cf.pop().inner_text()
+        print(href, cf1, cf2)
+    browser.close()
 
 
 def main():
-    _main()
+    __main()
 
 
 if __name__ == "__main__":
